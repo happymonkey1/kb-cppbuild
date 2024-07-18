56efbaaa42aa2edf6b1a1182e8acae0a93265a5d
happymonkey1 <happymonkey1@kablunk.com>
2024-07-18 00:20:42 -0700

    fix: fix vendor download and update gitignore

--- scripts/get-vendors.sh
+++ scripts/get-vendors.sh
@@ -2,4 +2,4 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 CATCH_VERSION=v3.6.0
 echo Downloading Catch2 $CATCH_VERSION to "$SCRIPT_DIR/vendor/catch2/include"
-(cd "$SCRIPT_DIR/../" && mkdir -p vendor/catch2/include && cd ./vendor/catch2/include && curl -o catch_amalgamated.hpp https://github.com/catchorg/Catch2/releases/download/$CATCH_VERSION/catch_amalgamated.hpp)
+(cd "$SCRIPT_DIR/../" && mkdir -p vendor/catch2/include && cd ./vendor/catch2/include && curl -LJO https://github.com/catchorg/Catch2/releases/download/$CATCH_VERSION/catch_amalgamated.hpp)
